14fb5272bf08fadd5171d6bb2eca82d1a4ca0976
Shad3 <122724277+LONELY-DUckWolf@users.noreply.github.com>
2025-04-19 18:26:48 +0300

    blloo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
+
+  const introText = document.getElementById("introText");
+  const introScreen = document.getElementById("introScreen");
+  const mainContent = document.getElementById("mainContent");
+
+  const message = "Hello that’s Vacuum!";
+  let index = 0;
+
+  function typeEffect() {
+    if (index < message.length) {
+      introText.textContent += message[index];
+      index++;
+      const glitchDelay = Math.random() * (100 - 30) + 30;
+      setTimeout(typeEffect, glitchDelay);
+    } else {
+      setTimeout(() => {
+        introScreen.style.opacity = 0;
+        setTimeout(() => {
+          introScreen.style.display = "none";
+          mainContent.classList.remove("hidden");
+          mainContent.classList.add("visible");
+        }, 1000);
+      }, 800);
+    }
+  }
+
+  window.addEventListener("DOMContentLoaded", () => {
+    typeEffect();
+  });
+
+
+
+
+
 const mainImg = document.querySelector(".main-img");
 const thumbs = document.querySelectorAll(".thumbs img");
 
